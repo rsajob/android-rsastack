6419edadc1c7cc2aa12ae9a956f622bbc3072d23
Roman Savelev <roman.savelev@gmail.com>
2019-03-25 11:48:30 +0300

    Remove full class name in NetworkModule

--- app/src/main/java/com/myapp/toothpick/NetworkModule.kt
+++ app/src/main/java/com/myapp/toothpick/NetworkModule.kt
@@ -2,11 +2,11 @@ package com.myapp.toothpick
 
 import com.myapp.domain.interactors.AuthData
 import com.myapp.domain.interactors.AuthDataPrefs
+import com.myapp.toothpick.providers.OkHttpProvider
 import com.rsastack.system.toothpick.module
 import okhttp3.OkHttpClient
-import toothpick.config.Module
 
 fun networkModule() = module {
     bind(AuthData::class.java).to(AuthDataPrefs::class.java).singletonInScope()
-    bind(OkHttpClient::class.java).toProvider(com.myapp.toothpick.providers.OkHttpProvider::class.java).providesSingletonInScope()
+    bind(OkHttpClient::class.java).toProvider(OkHttpProvider::class.java).providesSingletonInScope()
 }
