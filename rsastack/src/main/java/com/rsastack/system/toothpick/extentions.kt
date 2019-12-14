e31d2cb49a1182d0351c9290e1001a05d24db714
Roman Savelev <roman.savelev@gmail.com>
2019-12-14 21:32:53 +0300

    Toothpick: Added toothpickCreateInstance() for fast creating instances with extra bindings

--- rsastack/src/main/java/com/rsastack/system/toothpick/extentions.kt
+++ rsastack/src/main/java/com/rsastack/system/toothpick/extentions.kt
@@ -10,3 +10,16 @@ fun Scope.module(func: Module.() -> Unit): Scope {
     installModules(com.rsastack.system.toothpick.module { func(this) })
     return this
 }
+
+fun <T> toothpickCreateInstance(
+        scopeName: Any,
+        clazz: Class<T>,
+        bindings: Module.() -> Unit = {}
+): T
+{
+    val tmpScope = "tmpScope"
+    return Toothpick.openScopes(scopeName, tmpScope)
+            .module { bindings() }
+            .getInstance(clazz)
+            .also { Toothpick.closeScope(tmpScope) }
+}
