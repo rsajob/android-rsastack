0bfdf49e2eb3f362886330b344ed512a38cbd524
Roman Savelev <roman.savelev@gmail.com>
2021-07-21 06:36:00 +0300

    core: Возможность не выводить мия класса в логах (LoggerSettings.showSourceClassName)

--- rsastack/core/src/main/java/com/rsastack/system/utils/Logging.kt
+++ rsastack/core/src/main/java/com/rsastack/system/utils/Logging.kt
@@ -9,65 +9,78 @@ import com.rsastack.system.utils.LoggerSettings.logPrefix
 object LoggerSettings {
     var logPrefix = "deit/"
     var isLogEnabled = BuildConfig.BUILD_TYPE != "release"
+    var showSourceClassName = true
 }
 
 @SuppressLint("LogNotTimber")
 fun Any.debug(message: String, throwable: Throwable? = null) {
-    if (isLogEnabled)
-    Log.d(
-        logPrefix + if (javaClass.simpleName.isNotEmpty()) javaClass.simpleName else javaClass.name,
-        message ,
-        throwable
-    )
+    if (isLogEnabled) {
+        val sourceClassName = if (LoggerSettings.showSourceClassName) {if (javaClass.simpleName.isNotEmpty()) javaClass.simpleName else javaClass.name} else ""
+        Log.d(
+            logPrefix + sourceClassName,
+            message,
+            throwable
+        )
+    }
 }
 @SuppressLint("LogNotTimber")
 fun Any.info(message: String, throwable: Throwable? = null) {
     if (isLogEnabled)
-    Log.i(
-        logPrefix + if (javaClass.simpleName.isNotEmpty()) javaClass.simpleName else javaClass.name,
-        message ,
-        throwable
-    )
+    {
+        val sourceClassName = if (LoggerSettings.showSourceClassName) {if (javaClass.simpleName.isNotEmpty()) javaClass.simpleName else javaClass.name} else ""
+        Log.i(
+            logPrefix + sourceClassName,
+            message ,
+            throwable
+        )
+    }
 }
 
 @SuppressLint("LogNotTimber")
 fun Any.warn(message: String, throwable: Throwable? = null) {
-    if (isLogEnabled)
-    Log.w(
-        logPrefix + if (javaClass.simpleName.isNotEmpty()) javaClass.simpleName else javaClass.name,
-        message,
-        throwable
-    )
+    if (isLogEnabled) {
+        val sourceClassName = if (LoggerSettings.showSourceClassName) {if (javaClass.simpleName.isNotEmpty()) javaClass.simpleName else javaClass.name} else ""
+        Log.w(
+            logPrefix + sourceClassName,
+            message,
+            throwable
+        )
+    }
 }
 
 @SuppressLint("LogNotTimber")
 fun Any.verbose(message: String, throwable: Throwable? = null) {
-    if (isLogEnabled)
-    Log.v(
-        logPrefix + if (javaClass.simpleName.isNotEmpty()) javaClass.simpleName else javaClass.name,
-        message,
-        throwable
-    )
+    if (isLogEnabled) {
+        val sourceClassName = if (LoggerSettings.showSourceClassName) {if (javaClass.simpleName.isNotEmpty()) javaClass.simpleName else javaClass.name} else ""
+        Log.v(
+            logPrefix + sourceClassName,
+            message,
+            throwable
+        )
+    }
 }
 
 @SuppressLint("LogNotTimber")
 fun Any.err(message: String, throwable: Throwable? = null) {
-    if (isLogEnabled)
-    Log.e(
-        logPrefix + if (javaClass.simpleName.isNotEmpty()) javaClass.simpleName else javaClass.name,
-        logPrefix + message,
-        throwable
-    )
+    if (isLogEnabled){
+        val sourceClassName = if (LoggerSettings.showSourceClassName) {if (javaClass.simpleName.isNotEmpty()) javaClass.simpleName else javaClass.name} else ""
+        Log.e(
+            logPrefix + sourceClassName,
+            logPrefix + message,
+            throwable
+        )
+    }
 }
 
 @SuppressLint("LogNotTimber")
 fun Any.err(throwable: Throwable) {
     if (isLogEnabled) {
+        val sourceClassName = if (LoggerSettings.showSourceClassName) {if (javaClass.simpleName.isNotEmpty()) javaClass.simpleName else javaClass.name} else ""
         val message = " ${throwable.javaClass.simpleName} ${throwable.message}"
         Log.e(
-                logPrefix + if (javaClass.simpleName.isNotEmpty()) javaClass.simpleName else javaClass.name,
-                logPrefix + message,
-                throwable
+            logPrefix + sourceClassName,
+            logPrefix + message,
+            throwable
         )
     }
 }
