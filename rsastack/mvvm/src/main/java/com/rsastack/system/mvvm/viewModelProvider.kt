ea409d46357f1897e9d7f5a0a53bb850788257ed
Roman Savelev <roman.savelev@gmail.com>
2022-02-06 23:16:42 +0300

    [mvvm] Добавил упрощённый вариант inline fun Fragment.provideViewModel(scopeName)

--- rsastack/mvvm/src/main/java/com/rsastack/system/mvvm/viewModelProvider.kt
+++ rsastack/mvvm/src/main/java/com/rsastack/system/mvvm/viewModelProvider.kt
@@ -28,24 +28,37 @@ fun provideFactory(scopeName:String, cls:Class<*>, bindings: Module.() -> Unit =
     }
 }
 
-fun <T : ViewModel> Fragment.provideViewModel(scopeName:String, cls:Class<T>):T
+inline fun <reified T : ViewModel> Fragment.provideViewModel(scopeName:String, noinline bindings: Module.() -> Unit = {}):T
 {
-    val viewModelFactory = provideFactory(scopeName, cls)
-    return ViewModelProvider(this, viewModelFactory).get(cls)
+    val viewModelFactory = provideFactory(scopeName, T::class.java, bindings)
+    return ViewModelProvider(this, viewModelFactory)[T::class.java]
 }
 
+inline fun <reified T : ViewModel> AppCompatActivity.provideViewModel(scopeName:String, noinline bindings: Module.() -> Unit = {}):T
+{
+    val viewModelFactory = provideFactory(scopeName, T::class.java, bindings)
+    return ViewModelProvider(this, viewModelFactory)[T::class.java]
+}
+
+@Deprecated("Use inline method without cls:Class<T>", ReplaceWith("provideViewModel(scopeName)", "com.rsastack.system.mvvm"))
+fun <T : ViewModel> Fragment.provideViewModel(scopeName:String, cls:Class<T>):T =
+    ViewModelProvider(this, provideFactory(scopeName, cls))[cls]
+
+@Deprecated("Use inline method without cls:Class<T>", ReplaceWith("provideViewModel(scopeName, bindings)", "com.rsastack.system.mvvm"))
 fun <T : ViewModel> Fragment.provideViewModel(scopeName:String, cls:Class<T>, bindings: Module.() -> Unit = {}):T
 {
     val viewModelFactory = provideFactory(scopeName, cls, bindings)
     return ViewModelProvider(this, viewModelFactory).get(cls)
 }
 
+@Deprecated("Use inline method without cls:Class<T>", ReplaceWith("provideViewModel(scopeName, bindings)", "com.rsastack.system.mvvm"))
 fun <T : ViewModel> AppCompatActivity.provideViewModel(scopeName:String, cls:Class<T>):T
 {
     val viewModelFactory = provideFactory(scopeName, cls)
     return ViewModelProvider(this, viewModelFactory).get(cls)
 }
 
+@Deprecated("Use inline method without cls:Class<T>", ReplaceWith("provideViewModel(scopeName, bindings)", "com.rsastack.system.mvvm"))
 fun <T : ViewModel> AppCompatActivity.provideViewModel(scopeName:String, cls:Class<T>, bindings: Module.() -> Unit = {}):T
 {
     val viewModelFactory = provideFactory(scopeName, cls, bindings)
